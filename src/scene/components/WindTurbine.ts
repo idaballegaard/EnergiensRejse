3e7745c6e38c9975f0c564271567569e15fd434e
idaballegaard <idabal01@easv365.dk>
2026-04-27 13:20:32 +0200

    Added more windturbines

--- src/scene/components/WindTurbine.ts
+++ src/scene/components/WindTurbine.ts
@@ -3,48 +3,60 @@ import { GLTFLoader, type GLTF } from 'three/examples/jsm/loaders/GLTFLoader.js'
 import Landscape from './Landscape'
 
 const WIND_TURBINE_SCALE = 10
-const WIND_TURBINE_X = -20
-const WIND_TURBINE_Z = -20
 const WIND_TURBINE_Y_ROTATION = Math.PI / 2 + Math.PI
 
+// [x, z] positions for each turbine in the wind farm
+const WIND_TURBINE_POSITIONS: [number, number][] = [
+  [-22, -28],
+  [-22, -35],
+  [-30, -24],
+  [-30, -31],
+  [-30, -38],
+  [-38, -28],
+  [-38, -35],
+]
+
 export default class WindTurbine {
-  model: THREE.Group | null = null
-  private mixer: THREE.AnimationMixer | null = null
+  private mixers: THREE.AnimationMixer[] = []
   private clock = new THREE.Clock()
 
   constructor(scene: THREE.Scene) {
-    const loader = new GLTFLoader()
     const modelUrl = `${import.meta.env.BASE_URL}models/wind_turbine.glb`
 
-    loader.load(modelUrl, (gltf: GLTF) => {
-      this.model = gltf.scene
+    for (const [x, z] of WIND_TURBINE_POSITIONS) {
+      const loader = new GLTFLoader()
+      loader.load(modelUrl, (gltf: GLTF) => {
+        const model = gltf.scene
 
-      this.model.scale.setScalar(WIND_TURBINE_SCALE)
+        model.scale.setScalar(WIND_TURBINE_SCALE)
 
-      const groundY = Landscape.getHeight(WIND_TURBINE_X, WIND_TURBINE_Z)
-      this.model.position.set(WIND_TURBINE_X, groundY, WIND_TURBINE_Z)
-      this.model.rotation.y = WIND_TURBINE_Y_ROTATION
+        const groundY = Landscape.getHeight(x, z)
+        model.position.set(x, groundY, z)
+        model.rotation.y = WIND_TURBINE_Y_ROTATION
 
-      this.model.traverse((child) => {
-        if (child instanceof THREE.Mesh) {
-          child.castShadow = true
-          child.receiveShadow = true
+        model.traverse((child) => {
+          if (child instanceof THREE.Mesh) {
+            child.castShadow = true
+            child.receiveShadow = true
+          }
+        })
+
+        scene.add(model)
+
+        const clip = gltf.animations[0]
+        if (clip) {
+          const mixer = new THREE.AnimationMixer(model)
+          mixer.clipAction(clip).play()
+          this.mixers.push(mixer)
         }
       })
-
-      scene.add(this.model)
-
-      const clip = gltf.animations[0]
-      if (clip) {
-        this.mixer = new THREE.AnimationMixer(this.model)
-        this.mixer.clipAction(clip).play()
-      }
-    })
+    }
   }
 
   update() {
-    if (this.mixer) {
-      this.mixer.update(this.clock.getDelta())
+    const delta = this.clock.getDelta()
+    for (const mixer of this.mixers) {
+      mixer.update(delta)
     }
   }
 }
